b91cf3226b1c4eb9dcd3b3962fbe22fcbba29649
ilyes <ilyesssss34@gmail.com>
2025-08-16 22:30:13 +0100

    test

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 
+
 public class AnalyticsCounter {
 	private static int headacheCount = 0;	// initialize to 0
 	private static int rashCount = 0;		// initialize to 0
@@ -11,7 +12,7 @@ public class AnalyticsCounter {
 	
 	public static void main(String args[]) throws Exception {
 		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+		BufferedReader reader = new BufferedReader (new FileReader("Project02Eclipse\\symptoms.txt"));
 		String line = reader.readLine();
 
 		int i = 0;	// set i to 0
