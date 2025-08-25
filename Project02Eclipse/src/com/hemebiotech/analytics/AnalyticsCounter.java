66879797b72991199ab9e6bc38010a64e88bddda
ilyes <ilyesssss34@gmail.com>
2025-08-25 12:34:48 +0100

    Final

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -8,15 +8,32 @@ public class AnalyticsCounter {
 	public static void main(String[] args) throws IOException {
 		// first get input
 		ISymptomReader reader = new FileSymptomReader("Project02Eclipse/symptoms.txt");
-
 		try {
 			Map<String, Integer> symptoms = reader.readSymptoms();
+			//Initialize counters
+		 	int headacheCount = 0;
+		 	int rashCount = 0;
+		 	int pupilCount = 0;
 
 			for (Map.Entry<String, Integer> entry : symptoms.entrySet()) {
 				System.out.println(entry.getKey() + ": " + entry.getValue());
+
+			String symptom = entry.getKey();
+			int count = entry.getValue();
+
+			if (symptom.equalsIgnoreCase("headache")) {
+				headacheCount += count;
+			} else if (symptom.equalsIgnoreCase("rash")) {
+				rashCount += count;
+			} else if (symptom.equalsIgnoreCase("dilated pupils")) {
+				pupilCount += count;
 			}
 		}
-	catch (IOException e) {
+		System.out.println("Headache count: " + headacheCount);
+		System.out.println("Rash count: " + rashCount);
+		System.out.println("Dilated pupils count: " + pupilCount);
+
+	} catch (IOException e) {
 		System.err.println("Error reading symptoms: " + e.getMessage());
 		}
 	}
